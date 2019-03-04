ab130a36b457c157061e2a5bc74a20aa134ccafb
John Nezzi <johnnezzi@yahoo.com>
2019-03-04 21:55:43 +0000

    initial commit

---
+++ newwebsite/src/components/landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Landing = () => {
+  return ( 
+    <div className='landing'>
+        <h1>Hi, I'm John</h1>
+        <h2>I'm a full Stack Web Developer</h2>
+
+    </div>
+
+  
+   );
+}
+ 
+export default Landing;
