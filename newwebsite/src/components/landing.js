9b9bb7400af737eabdb94a743b7ec897f1509e16
John Nezzi <johnnezzi@yahoo.com>
2019-03-04 22:09:55 +0000

    add Landing.css file

--- newwebsite/src/components/landing.js
+++ newwebsite/src/components/landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import '../css/Landing.css'
 
 const Landing = () => {
   return ( 
     <div className='landing'>
+      <div className='text'>
         <h1>Hi, I'm John</h1>
         <h2>I'm a full Stack Web Developer</h2>
-
+      </div>
     </div>
 
   
