ce289a590a93c13d0161d1e16dbfbdfe2f4ca07e
John Nezzi <johnnezzi@yahoo.com>
2019-03-12 10:48:20 +0000

    added community gems

--- newwebsite/src/components/Portfolio.js
+++ newwebsite/src/components/Portfolio.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import JSNewsIMG from '../assets/JSNews.jpg'
 import CTIMG from '../assets/Crimetime.jpg'
+import ComGemIMG from '../assets/ComGems.jpg'
 import '../css/Portfolio.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 class Portfolio extends Component {
-  state = { 
+  state = {
     pieces: [
       {
         title: "JSN News",
@@ -15,9 +16,15 @@ class Portfolio extends Component {
         link: "https://jsnews.netlify.com/",
         image: JSNewsIMG
         },
+        {
+        title: "Community Gems (Team Project)",
+        description: "A react native mobile app built with Expo. Database, Auth and messaging built with Firebase. Community Gems allows you to create, search for and sign for volunteering events in your local area.",
+        link: "https://www.youtube.com/watch?v=qZ-dZrjwnuE&list=PLcwpumAUZQkoZ66NZ5Uk5yztjrifIQTMd&index=2&t=0s",
+        image: ComGemIMG
+        },
       {
         title: "Crimetime",
-        descritption: " lsnlsakdnlasndla lslasn khfaks fuksnbf kjdshf kjdsfkjkdheueryoiuen,m  lnmlksdfhuhe ldfoue kjdclknsfou ldlsfnjhsdsfldf",
+        description: "This is the first app I built in React.js, although simple in its front end, this excercise concentrated making requests to third party APIs (in this case the police crime database) and serving up the results ",
         link: "test link",
         image: CTIMG
       }
